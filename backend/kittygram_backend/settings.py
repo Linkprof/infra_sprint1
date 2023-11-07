45130ed643ace5e53e9e8b6bee5bb39adf2fa083
Linkprof <off.nemir.off@mail.ru>
2023-11-07 03:13:12 +0000

    3

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 load_dotenv()
-SECRET_KEY = os.getenv['SECRET_KEY']
+SECRET_KEY = os.getenv('KEY')
 
 DEBUG = False
 
